365b612db181abae3b787bf69b317b3ade735175
feendres <89131994+feendres@users.noreply.github.com>
2026-01-28 11:22:53 +0100

    Update options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,28 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 1. Always use system clipboard behavior
+vim.opt.clipboard = "unnamedplus"
+
+-- 2. Define the 'Anti-Freeze' Paste Function
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg('"'), "\n"),
+    vim.fn.getregtype('"'),
+  }
+end
+
+-- 3. FORCE the OSC 52 provider (No "if" checks)
+--    This uses the internal Neovim v0.10+ module
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = paste,
+    ['*'] = paste,
+  },
+}
