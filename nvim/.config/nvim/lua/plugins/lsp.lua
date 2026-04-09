6a9808a8372f364988a343e099b8ffb9e7de964e
feendres <89131994+feendres@users.noreply.github.com>
2026-04-09 11:49:21 +0200

    Update lsp.lua

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,12 @@ return {
             basedpyright = {
               analysis = {
                 typeCheckingMode = "standard",
+                diagnosticSeverityOverrides = {
+                  reportAny = "none",
+                  reportUnknownMemberType = "none",
+                  reportUnknownVariableType = "none",
+                  reportUnknownArgumentType = "none",
+                },
               },
             },
           },
