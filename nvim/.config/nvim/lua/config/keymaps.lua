2d655401c110ef1db2ed1340390e2b26f0ad85d1
feendres <89131994+feendres@users.noreply.github.com>
2026-05-04 14:30:46 +0200

    Update keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Delete single character without copying into register
+vim.keymap.set("n", "x", '"_x')
+
+-- Leader+d to delete without copying
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
