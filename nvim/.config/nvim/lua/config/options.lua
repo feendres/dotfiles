2d263c654e8415fc131f3004464727b70fe4ea0e
feendres <89131994+feendres@users.noreply.github.com>
2026-05-04 14:37:04 +0200

    Update options.lua

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.clipboard = 'osc52'
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
